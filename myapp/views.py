56527fcb575dad1651f163065342f198f0e02a95
allen <allenac205@gmail.com>
2022-10-09 18:50:40 +0530

    9th oct 2022

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,15 @@
+from contextvars import Context
+import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello new world')
+    li = ['allen','sam','savis']
+    context = {'names':li}
+    return render(request, 'listing/index.html', context = context)
+
+def new_one(request):
+    return HttpResponse('this is new one')
